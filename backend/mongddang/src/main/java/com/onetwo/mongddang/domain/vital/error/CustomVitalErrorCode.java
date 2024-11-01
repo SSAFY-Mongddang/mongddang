c6572edcb529e43c213db0a45254acf4eb035118
나혜림 <rimhyena@gmail.com>
2024-11-01 17:55:12 +0900

    :sparkles: feat/(#41, S11P31D207-176, S11P31D207-177): CustomVitalErrorCode 추가

--- backend/mongddang/src/main/java/com/onetwo/mongddang/domain/vital/error/CustomVitalErrorCode.java
+++ backend/mongddang/src/main/java/com/onetwo/mongddang/domain/vital/error/CustomVitalErrorCode.java
@@ -9,7 +9,7 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum CustomVitalErrorCode implements ErrorCode {
    THIS_USER_VITAL_LOG_NOT_FOUND(HttpStatus.NOT_FOUND, "VT000","해당 유저의 바이탈 정보를 찾을 수 없습니다."),
-   THIS_DATE_IS_NOT_VALID(HttpStatus.NOT_FOUND, "VT000","해당 유저의 바이탈 정보를 찾을 수 없습니다."),
+   THIS_DATE_IS_NOT_VALID(HttpStatus.NOT_FOUND, "VT003","미래날짜의 바이탈 정보는 찾을 수 없습니다."),
     ;
 
     private final HttpStatus httpStatus;
